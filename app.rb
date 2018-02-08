0474c2b50821ebd6a3c4d813b64cf2f69963cd89
David Kuruvilla <dapawn@gmail.com>
2018-02-08 21:19:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 class App < Sinatra::Base
+
+  get '/' do
+  erb :index
+end
+
+post '/checkout' do
+  session["name"] = params[:item]
+  @session = session
+
+  erb :checkout
+end
+
 end
